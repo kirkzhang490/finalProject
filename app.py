cd3d94be004df757554dbdff918e7eeafec9ae93
kirkzhang49 <314698267@qq.com>
2021-10-30 19:47:04 -0700

    user logic and ui and search

--- app.py
+++ app.py
@@ -1,9 +1,74 @@
-from flask import Flask
+from flask import Flask,request,render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+import module
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+categories = ['Technology', 'Food', 'Entertainment', 'Animation', 'Outdoor', 'BoardGame', 'Sport', 'Investment']
+
+@app.route('/insert_user')
+def insert_user():
+    name = request.args.get('name')
+    categories = request.args.get('categories')
+    feedback = request.args.get('feedback')
+    tb = module.Users()
+    result = tb.create(name,categories,feedback)
+    return result
+
+@app.route('/clear_all_user')
+def drop_all_user():
+    tb = module.Users()
+    tb.dropTable()
+    return "emptyUserTableNow"
+
+##/update_user?name=zhengkai%20zhang&categories=Technology,Food,Entertainment&feedback=pokemon
+@app.route('/update_user')
+def update_user():
+    name = request.args.get('name')
+    categories = request.args.get('categories')
+    feedback = request.args.get('feedback')
+    tb = module.Users()
+    return tb.updateUser(name,categories,feedback)
+
+@app.route('/', methods=['GET','POST'])
+def root():
+    errors = []
+    result = ''
+    allUsers = []
+    try:
+        tb = module.Users()
+        allUsers = tb.allUsers()
+    except:
+        errors.append('not able to fetch user db')
+    if request.method == "POST":
+        try:
+            url = request.form['url']
+            user = request.form['user']
+            result = user + ': ' + url
+        except:
+            errors.append(
+                "Unable to get URL. Please make sure it's valid and try again."
+            )
+    return render_template('index.html', errors=errors, result=result,allUsers=allUsers)
+
+@app.route('/user', methods=['GET','POST'])
+def userPage():
+    errors = []
+    result = ''
+    if request.method == "POST":
+        try:
+            categoriesForUser = request.form.getlist('categories')
+            categoriesForUser = ','.join(categoriesForUser)
+            userName = request.form.get('userName')
+            tb = module.Users()
+            result = tb.create(userName,categoriesForUser,'')
+            print(result)
+        except:
+            errors.append(
+                "Unable add user name may duplicated."
+            )
+    return render_template('user.html',categories=categories, result=result, errors=errors)
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8000, debug=True)
+    app.run()
---
+++ module.py
@@ -0,0 +1,86 @@
+from sqlalchemy import create_engine  
+from sqlalchemy import Table,Column, String,Integer
+from sqlalchemy import select,delete, update
+from sqlalchemy.ext.declarative import declarative_base  
+from sqlalchemy.orm import sessionmaker
+
+
+db_string = "postgresql://postgres:kaikai49@localhost:5430"
+
+db = create_engine(db_string)  
+base = declarative_base()
+Session = sessionmaker(db)  
+session = Session()
+
+class Users(base):  
+    __tablename__ = 'users'
+    id =  Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+    categories = Column(String)
+    feedback = Column(String)
+
+    def create(self, name, categories, feedback):
+        result =  session.query(Users).filter(Users.name==name).first()
+        if result is not None:
+            return 'duplicated name try again'
+        try:
+            stmt = Users(name=name, categories=categories, feedback=feedback)  
+            session.add(stmt)
+            session.commit()
+        except:
+            return 'not able to create this time'
+        return self.queryByName(name)
+        
+    
+    def queryByName(self, name):
+        result =  session.query(Users).filter(Users.name==name).one()
+        return 'Created name: ' + result.name + ' categories: ' + result.categories + ' feedback: ' + result.feedback + 'id: ' + str(result.id)
+    
+    def dropTable(self):
+        session.query(Users).delete()
+        session.commit()
+
+    def allUsers(self):
+        users_ = session.query(Users)
+        result = []
+        for user in users_:  
+            result.append({
+                'name': user.name,
+                'categories': user.categories,
+                'feedback': user.feedback
+            })
+        print(result)
+        return result
+    
+    def updateUser(self,name, categories, feedback):
+        check =  session.query(Users).filter(Users.name==name).first()
+        if check is None:
+            return 'user not exist'
+        try:
+            session.query(Users).filter(Users.name == name).update({"categories": categories, 'feedback': feedback})
+            session.commit()
+        except:
+            return 'not able to update this time'
+        return 'success updated'
+
+# # Delete
+# stmt = delete(Users).where(Users.name == 'Doctor Strange')
+# result = session.execute(stmt)
+# session.commit()
+
+# Create 
+# doctor_strange = Users(name="Doctor Strange", categories="Scott Derrickson", feedback="2016")  
+# session.add(doctor_strange)  
+# session.commit()
+
+#update
+# stmt = update(Users).where(Users.name == 'kirk Strange').values(name = "Some2016Film")
+# result = session.execute(stmt)
+# session.commit()
+
+users_ = session.query(Users)  
+for user in users_:  
+    print(user.name)
+
+# Users.__table__.drop(db)
+# base.metadata.create_all(db)    
